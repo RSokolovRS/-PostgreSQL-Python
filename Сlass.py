6652c6687f981a043ab3b120e2745fd90d6a0bce
RSokolov <roman-27@inbox.ru>
2023-04-05 16:26:17 +0300

    change

--- Сlass.py
+++ Сlass.py
@@ -1,30 +1,49 @@
-import psycopg2 
+import psycopg2
 
-class CustomersDB:
-    def __init__(self, nameDB, userDB, passwordDB, query):
-        self.nameDB = nameDB
-        self.userDB = userDB
-        self.passwordDB = passwordDB
-        self.query = query
-
-    def connect_create(self):
-        connection = None
-        try: 
-            connection = psycopg2.connect(
-                database=self.nameDB, 
-                user=self.userDB,
-                password=self.passwordDB)
-            print(f'Удачное подключение!')
-        except ConnectionError as CE:
-            print(f'Ошибка подключения {CE}')
-        return connection
-
-    def 
+def create_db(conn):
+    conn
+    print(f'Удачное подключение!')
 
 
-class 
+def add_client(conn, cur, name, surname, email, customer_id, phones=None):
+    query = ("""INSERT INTO Customers(first_name, last_name) VALUES
+        (%s, %s); INSERT INTO email(email, customer_id) VALUES
+        (%s, %s);""")
+    paramas = (name, surname, email, customer_id)
+    cur.execute(query,paramas)
+    conn.commit()
+    print('Данные успешно добавлены!')
+   
+
+def add_phone(conn, cur, customer_id, number):
+    query = ("""INSERT INTO phone_numbers(customer_id, number) VALUES
+        (%s, %s);""")
+    par = (customer_id, number,)
+    cur.execute(query,par)
+    conn.commit()
+    print(f'Успешно!')
+
+    
+
+def change_client(conn, cur,  client_id, first_name=None, last_name=None, email=None, phones=None):
+    sql = ("""UPDATE customers SET first_name =%s WHERE first_name =%s;""")
+    par = ()
 
 
+def delete_phone(conn, client_id, phone):
+    pass
 
-connect = CustomersDB('CustomersDB', 'postgres', '05121978')
-connect.connect_create()
+def delete_client(conn, client_id):
+    pass
+
+def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
+    pass
+
+
+with psycopg2.connect(database="CustomersDB", user="postgres", password="05121978") as conn:
+    create_db(conn)  # вызывайте функции здесь
+    with conn.cursor() as cur:
+        # add_client(conn, cur, 'Roman', 'Sokolov', 'r@mail.ru', 5)
+        # add_phone(conn, cur, 5, 79205855989)
+
+conn.close()
